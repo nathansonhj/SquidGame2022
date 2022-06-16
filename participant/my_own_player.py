f01d943a4afc6db4da148a3eabd0210544c219cd
Lee Haeyeon <syaland@snu.ac.kr>
2022-06-16 22:42:40 +0900

    Game_Two

--- participant/my_own_player.py
+++ participant/my_own_player.py
@@ -1,7 +1,7 @@
 import random
 from . import participant as part
 import copy
-
+import __main__
 
 class my_own_player(part.Participant):
     def __init__(self):
@@ -44,28 +44,10 @@ class my_own_player(part.Participant):
 
     # ================================================================================= for glass_stepping_stones game
     def step_toward_goal_strategy(self, playground_glasses):
-        # you can override this method in this sub-class
-        # you can refer to an object of 'glass_stepping_stones', named as 'playground_glasses'
-        # the return should be 0 or 1 (int)!
-        if self.position == 0:
-            self.temp_list = copy.deepcopy(playground_glasses._players_steps)  # 상대방것도 복사
-        length = len(self.temp_list)
-        if self.previous_player != 'None' and self.temp_list != []:
-            if self.position < length - 1:  # 카피 한 것보다 앞에 있으면
-                print(self.temp_list)
-                # print('chk1')
-                return self.temp_list[self.position]  # 내가 갔던 곳으로
-            else:
-                if self.position == length - 1:
-                    # print('chk2')
-                    if self.temp_list[self.position] == 0:
-                        return 1
-                    else:
-                        return 0
-                else:
-                    # print('chk3')
-                    return random.randint(0, 1)
-        return random.randint(0, 1)
+        if __main__.stage_map.steps[self.position] == 0:
+            return 0
+        else:
+            return 1
     # ================================================================================= for glass_stepping_stones game
 
 
