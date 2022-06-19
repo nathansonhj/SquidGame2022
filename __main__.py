9cbd38c1bf887830f60fac450d9f135d49367377
mestherr <mesther@snu.ac.kr>
2022-06-19 12:10:15 +0900

    Final Game - Esther

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ import glass_stepping_stones.game as r3
 from participant import computer as com
 from participant import my_own_player as player
 
-# For Game_Two
+# Final_Game
 
 def report(winner, team, game):
     with open('result_term_project.csv', 'a') as f:  # the file name may change
@@ -28,25 +28,25 @@ if __name__ == '__main__':
     print("\n □ : 모든 참가자가 성공적으로 입장하였습니다.\n")
 
 
-    #print("\n □ : 홀짝게임을 먼저 시작합니다.\n")
-    #for i in range(5):
-    #    print("\n □ : {}번째 게임을 시작합니다.\n".format(i + 1))
-    #    stage = r1.marbles(players)
-    #    winner_name = stage.run_game(players)
-    #    report(winner_name, team, 'marble')
+    print("\n □ : 홀짝게임을 먼저 시작합니다.\n")
+    for i in range(5):
+        print("\n □ : {}번째 게임을 시작합니다.\n".format(i + 1))
+        stage = r1.marbles(players)
+        winner_name = stage.run_game(players)
+        report(winner_name, team, 'marble')
 
-    # print("\n □ : 다음으로 줄다리기 게임을 시작합니다.\n")
-    # for i in range(1):
-    #     print("\n □ : {}번째 게임을 시작합니다.\n".format(i + 1))
-    #     stage_map = r2.Map(5)
-    #     stage = r2.tug_of_war(stage_map, players)
-    #     winner_name = stage.run_game(players, stage_map)
-    #     report(winner_name, team, 'Tug')
+    print("\n □ : 다음으로 줄다리기 게임을 시작합니다.\n")
+    for i in range(5):
+        print("\n □ : {}번째 게임을 시작합니다.\n".format(i + 1))
+        stage_map = r2.Map(5)
+        stage = r2.tug_of_war(stage_map, players)
+        winner_name = stage.run_game(players, stage_map)
+        report(winner_name, team, 'Tug')
 
     print("\n □ : 마지막으로 징검다리 게임을 시작합니다.\n")
     for i in range(5):
-         print("\n □ : {}번째 게임을 시작합니다.\n".format(i + 1))
-         stage_map = r3.Map(20)
-         stage = r3.glass_stepping_stones(stage_map, players)
-         winner_name = stage.run_game(stage_map)
-         report(winner_name, team, 'glass')
+        print("\n □ : {}번째 게임을 시작합니다.\n".format(i + 1))
+        stage_map = r3.Map(20)
+        stage = r3.glass_stepping_stones(stage_map, players)
+        winner_name = stage.run_game(stage_map)
+        report(winner_name, team, 'glass')
--- participant/my_own_player.py
+++ participant/my_own_player.py
@@ -1,40 +1,23 @@
 import random
 from . import participant as part
-import copy
 import __main__
 
 class my_own_player(part.Participant):
     def __init__(self):
         super().__init__('Son', 'Team 03')
-        # you can change everything in this code file!!
-        # also, you can define your own variables here or in the overriding method
-        # Any modifications are possible if you follows the rules of Squid Game
 
 
     # ====================================================================== for initializing your player every round
     def initialize_player(self, string):
-        # you can override this method in this sub-class
-        # this method must contain 'self.initialize_params()' which is for initializing some essential variables
-        # you can initialize what you define
         self.initialize_params()
     # ====================================================================== for initializing your player every round
 
 
     # ================================================================================= for marble game
     def bet_marbles_strategy(self, playground_marbles):
-        # you can override this method in this sub-class
-        # you can refer to an object of 'marbles', named as 'playground_marbles'
-        # the return should be the number of marbles bet (> 0)!
-        # my_current_marbles = playground_marbles.get_num_of_my_marbles(self)
-        # return random.randint(playground_marbles.MIN_HOLDING, my_current_marbles)
         return random.randint(1, 2)
 
     def declare_statement_strategy(self, playground_marbles):
-        # you can override this method in this sub-class
-        # you can refer to an object of 'marbles', named as 'playground_marbles'
-        # the return should be True or False!
-        # answer = bool(random.randint(0, 1))
-        # return self.set_statement(answer)
         if playground_marbles._marbles_in_hand % 2 == 1:
             self.set_statement(True)
         else:
@@ -53,7 +36,6 @@ class my_own_player(part.Participant):
 
     # ================================================================================= for tug_of_war game
     def gathering_members(self):
-
         return (0, 0, 10, 0)
 
     def act_tugging_strategy(self, playground_tug_of_war):
